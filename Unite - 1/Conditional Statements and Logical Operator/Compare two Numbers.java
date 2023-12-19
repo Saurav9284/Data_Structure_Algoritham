0b0217f2a51e935cb9459cb5747cff2ca9c22d79
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 18:15:25 +0530

    Create Compare two Numbers.java

---
+++ Unite - 1/Conditional Statements and Logical Operator/Compare two Numbers.java
@@ -0,0 +1,28 @@
+public static void compareTwoNumbers(int num1,int num2){
+      
+      if(num1>num2)
+      {
+          System.out.println("true");
+      }
+      else
+      {
+          System.out.println("false");
+      }
+      if(num1<num2)
+      {
+          System.out.println("true");
+      }
+      else
+      {
+          System.out.println("false");
+      }
+      if(num1==num2)
+      {
+          System.out.println("true");
+      }
+      else
+      {
+          System.out.println("false");
+      }
+      
+  }
