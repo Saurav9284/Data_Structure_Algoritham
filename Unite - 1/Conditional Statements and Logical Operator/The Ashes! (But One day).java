6c102c3869a42b6802862adcb0b428bd4f9a0934
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 18:19:51 +0530

    Create The Ashes! (But One day).java

---
+++ Unite - 1/Conditional Statements and Logical Operator/The Ashes! (But One day).java
@@ -0,0 +1,17 @@
+public static void theAshes(int Australia,int England){
+       
+       if(Australia>England)
+       {
+           System.out.println("Australia");
+       }
+       else if(Australia<England)
+       {
+           System.out.println("England");
+       }
+       else if(Australia==England)
+       {
+           System.out.println("Tie");
+       }
+      
+    
+  }
