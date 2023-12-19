41d77dac2fcbb3d8159474bdac7cd44cce318491
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 18:18:35 +0530

    Create Divisible by 4.java

---
+++ Unite - 1/Conditional Statements and Logical Operator/Divisible by 4.java
@@ -0,0 +1,11 @@
+public static void divisibleBy4(int n){
+      
+      if(n%4==0)
+      {
+          System.out.println("Yes");
+      }
+      else
+      {
+          System.out.println("No");
+      }
+  }
