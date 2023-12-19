a879b1313cb9ed3c8608580349c9faa401541a35
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 18:19:09 +0530

    Create Easy Wood Cutter.java

---
+++ Unite - 1/Conditional Statements and Logical Operator/Easy Wood Cutter.java
@@ -0,0 +1,11 @@
+public static void divisibleBy3(int n){
+      
+      if(n%3==0)
+      {
+          System.out.println("Yes");
+      }
+      else
+      {
+          System.out.println("No");
+      }
+ }
