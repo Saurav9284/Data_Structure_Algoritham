c0df1b64624cc5610ca461eddbe815f925e325a1
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 17:53:47 +0530

    Create Seven Numbers.java

---
+++ Unite - 1/Mathematical Operator and Comparision Operator/Seven Numbers.java
@@ -0,0 +1,7 @@
+public static void sevenNumbers(int a,int b,int c,int d,int e,int f,int g){
+        
+        int one = a+b+c;
+        int two = d+e+f+g;
+        
+        System.out.println(one*two);
+    }
