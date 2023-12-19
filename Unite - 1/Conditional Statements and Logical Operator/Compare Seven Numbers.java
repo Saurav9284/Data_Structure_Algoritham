59da4fdb4b985a9b5ecbea3227ceba480683b557
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 18:16:39 +0530

    Create Compare Seven Numbers.java

---
+++ Unite - 1/Conditional Statements and Logical Operator/Compare Seven Numbers.java
@@ -0,0 +1,14 @@
+public static void compareSevenNumbers(int a,int b,int c,int d,int e,int f,int g){
+    
+      int sum1=(a+b)*c;
+      int sum2=(d+e+f+g);
+      
+      if(sum1>sum2)
+      {
+          System.out.println("true");
+      }
+      else
+      {
+          System.out.println("false");
+      }
+  }
