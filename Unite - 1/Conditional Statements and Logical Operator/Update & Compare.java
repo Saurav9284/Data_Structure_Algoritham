144034d7c4a95aebde47da04081d7127c0082955
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 18:14:46 +0530

    Create Update & Compare.java

---
+++ Unite - 1/Conditional Statements and Logical Operator/Update & Compare.java
@@ -0,0 +1,22 @@
+public static void updateCompare(int num1,int num2,int num3){
+    
+       if(num1>num2)
+       {
+           System.out.println("true");
+       }
+       else
+       {
+           System.out.println("false");
+       }
+        
+        num1=num1+num3;
+        if(num1>num2)
+       {
+           System.out.println("true");
+       }
+       else
+       {
+           System.out.println("false");
+       }
+        
+  }
