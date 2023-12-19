d3050975d0906c5f286744a65d2a8772f92b0c59
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 18:20:43 +0530

    Create Masai Even or Odd.java

---
+++ Unite - 1/Conditional Statements and Logical Operator/Masai Even or Odd.java
@@ -0,0 +1,12 @@
+public static void masaiEvenOrOdd(int n){
+      
+      if(n%2==0)
+      {
+          System.out.println("Even");
+      }
+      else
+      {
+          System.out.println("Odd");
+      }
+    
+  }
