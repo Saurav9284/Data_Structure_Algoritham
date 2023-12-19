6257abdcee5e2be354cd5c736675928a147a0700
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 18:17:54 +0530

    Create Cube and Square.java

---
+++ Unite - 1/Conditional Statements and Logical Operator/Cube and Square.java
@@ -0,0 +1,16 @@
+public static void cubeAndSquare(int n,int m){
+      
+      int cube = n*n*n;
+      int square = m*m;
+      
+      if(cube > square)
+      {
+          System.out.println("true");
+          
+      }
+      else
+      {
+          
+          System.out.println("false");
+      }
+  }
