33db6c7c714fdfacf4bf8e7aedbde164c4803d1f
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 18:16:01 +0530

    Create Sum & Compare.java

---
+++ Unite - 1/Conditional Statements and Logical Operator/Sum & Compare.java
@@ -0,0 +1,14 @@
+public static void sumAndCompare(int one,int two,int three,int four,int five){
+    
+      int sum1=one+two+three;
+      int sum2=four+five;
+      
+      if(sum1>sum2)
+      {
+          System.out.println("true");
+      }
+      else
+      {
+          System.out.println("false");
+      }
+  }
