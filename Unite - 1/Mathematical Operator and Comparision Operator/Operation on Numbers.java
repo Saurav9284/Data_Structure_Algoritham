a7b11e5aaeac72dd4195abed6a108432b19d71f9
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 17:51:09 +0530

    Create Operation on Numbers.java

---
+++ Unite - 1/Mathematical Operator and Comparision Operator/Operation on Numbers.java
@@ -0,0 +1,8 @@
+public static void operationOnNumbers(int number){
+        
+        int mul = number*3;
+        int add = mul+7;
+        int sub = add-10;
+        
+        System.out.println(sub);
+    }
