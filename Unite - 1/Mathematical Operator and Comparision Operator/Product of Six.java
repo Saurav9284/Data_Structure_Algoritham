570795a51c8642a51cc52336b2894dbe644773e2
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-19 17:47:31 +0530

    Create Product of Six.java

---
+++ Unite - 1/Mathematical Operator and Comparision Operator/Product of Six.java
@@ -0,0 +1,6 @@
+public static void productOfSix(int one,int two,int three,int four,int five,int six){
+      
+      int Product=one*two*three*four*five*six;
+      
+      System.out.println(Product);
+  }
