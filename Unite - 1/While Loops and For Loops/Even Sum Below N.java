5e6b60724421b9cdf57b2fd52c54bd513e735c49
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-20 11:13:48 +0530

    Create Even Sum Below N.java

---
+++ Unite - 1/While Loops and For Loops/Even Sum Below N.java
@@ -0,0 +1,13 @@
+public static void evenSumBelowN(int num){
+      
+      int sum=0;
+      
+      for(int i=1; i<=num; i++)
+      {
+          if(i%2==0)
+          {
+             sum=sum+i;
+          }
+      }
+      System.out.println(sum);
+  }
