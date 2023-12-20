afb83d818829178c427a6261d0813263480b5836
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-20 11:16:43 +0530

    Update Find Sum of Multiples.java

--- Unite - 1/While Loops and For Loops/Find Sum of Multiples.java
+++ Unite - 1/While Loops and For Loops/Find Sum of Multiples.java
@@ -1,1 +1,14 @@
 
+public static void findSumOfMultiples(int x,int k,int y){
+    int sum=0;
+    
+    for(int i=1; i<=k; i++)
+    {   int mul=x*i;
+        if(mul%y==0)
+        {
+            sum=sum+mul;
+        }
+    }
+    System.out.println(sum);
+      
+  }
