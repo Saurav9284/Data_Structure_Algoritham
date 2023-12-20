56b22859d5b83ccf62d9f72f0d322855831aed86
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-20 11:13:15 +0530

    Create Move 2 Ahead II.java

---
+++ Unite - 1/While Loops and For Loops/Move 2 Ahead II.java
@@ -0,0 +1,10 @@
+public static void moveTwoAheadII(int num){
+    
+     for(int i=1; i<=num; i++)
+     {
+         if(i%2==0)
+         {
+             System.out.println(i);
+         }
+     }
+  }
