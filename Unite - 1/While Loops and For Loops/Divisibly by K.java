fdb4080d99afe0317d13632329d2ffb91a81f971
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-20 11:17:47 +0530

    Create Divisibly by K.java

---
+++ Unite - 1/While Loops and For Loops/Divisibly by K.java
@@ -0,0 +1,10 @@
+public static void divisibleByK(int num,int k){
+    for(int i=1; i<=num; i++)
+    {
+        if(i%k==0)
+        {
+            System.out.println(i);
+        }
+    }
+    
+  }
