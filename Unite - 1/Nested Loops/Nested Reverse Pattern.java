53e29f0df3956c76a4c8d8c8e6b6aa8b6df4cd0b
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-21 11:37:01 +0530

    Create Nested Reverse Pattern.java

---
+++ Unite - 1/Nested Loops/Nested Reverse Pattern.java
@@ -0,0 +1,12 @@
+public static void solve(int num){
+    
+     for(int i=1; i<=num; i++)
+     {
+         for(int j=num; j>=1; j--)
+         {
+             System.out.print(j+" ");
+         }
+         
+         System.out.println();
+     }
+  }
