78997aa3d3dca02afbca52dd3c043ce646982cd2
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-21 11:31:48 +0530

    Create Your First Pattern.java

---
+++ Unite - 1/Nested Loops/Your First Pattern.java
@@ -0,0 +1,12 @@
+public static void yourFirstPattern(int num){
+       
+       for(int i=1; i<=num; i++)
+       {
+           for(int j=1; j<=num; j++)
+           {
+               System.out.print("* ");
+           }
+           
+            System.out.println();
+       }
+  }
