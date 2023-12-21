ab902713cdd9e57ecd322bb951b4047661c78006
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-21 11:31:18 +0530

    Create Your first nested loop.java

---
+++ Unite - 1/Nested Loops/Your first nested loop.java
@@ -0,0 +1,12 @@
+public static void yourFirstNested(int num){
+      
+      for(int i=1; i<=num; i++)
+      {
+          for(int j=1; j<=num; j++)
+          {
+              System.out.print(j+" ");
+          }
+          
+          System.out.println();
+      }
+  }
