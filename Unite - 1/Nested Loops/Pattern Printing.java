22c727b8dac3d22171e1051457c144a98bffcb1b
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-21 11:35:09 +0530

    Create Pattern Printing.java

---
+++ Unite - 1/Nested Loops/Pattern Printing.java
@@ -0,0 +1,12 @@
+public static void patternPrinting(int num){
+      
+      for(int i=1; i<=num; i++)
+      {
+          for(int j=1; j<=i; j++)
+          {
+              System.out.print("*");
+          }
+          
+          System.out.println();
+      }
+  }
