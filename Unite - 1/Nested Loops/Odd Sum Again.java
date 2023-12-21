5a8e2db2493415819aed4326c42e99128505508a
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-21 11:33:49 +0530

    Create Odd Sum Again.java

---
+++ Unite - 1/Nested Loops/Odd Sum Again.java
@@ -0,0 +1,16 @@
+public static void oddSumAgain(int num){
+      for(int i=1; i<=num; i++)
+      {
+          int sum = 0;
+          
+          for(int j=1; j<=i; j++)
+          {
+              if(j%2!=0)
+              {
+                  sum=sum+j;
+              }
+          }
+          
+          System.out.println(sum);
+      }
+  }
