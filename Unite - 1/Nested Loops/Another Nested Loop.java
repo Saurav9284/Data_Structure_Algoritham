5ca9ac5ca13b570a08492e7611ec0a1bd5a676d3
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-21 11:32:23 +0530

    Create Another Nested Loop.java

---
+++ Unite - 1/Nested Loops/Another Nested Loop.java
@@ -0,0 +1,10 @@
+public static void anotherNested(int num){
+      
+      for(int i=1; i<=num; i++)
+      {
+          for(int j=1; j<=i; j++)
+          {
+              System.out.println(j);
+          }
+      }
+  }
