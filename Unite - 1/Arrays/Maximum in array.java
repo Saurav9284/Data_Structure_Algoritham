f932149b4e23ab8967fcba3d1c47e001feaa3d7e
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-22 13:24:50 +0530

    Create Maximum in array.java

---
+++ Unite - 1/Arrays/Maximum in array.java
@@ -0,0 +1,14 @@
+public static void maximumInArray(int n, int arr[]){
+      
+      int max = arr[0];
+      
+      for(int i=0; i<n; i++)
+      {
+          if(arr[i]>max)
+          {
+              max=arr[i];
+          }
+      }
+      
+      System.out.println(max);
+  }
