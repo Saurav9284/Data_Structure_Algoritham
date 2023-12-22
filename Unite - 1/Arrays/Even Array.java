64c9f8ff1d8b88788820d8a091ee015e1d4dd32d
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-22 13:25:50 +0530

    Create Even Array.java

---
+++ Unite - 1/Arrays/Even Array.java
@@ -0,0 +1,10 @@
+public static void evenArray(int n, int arr[]){
+       
+       for(int i=0; i<n; i++)
+       {
+           if(arr[i]%2==0)
+           {
+               System.out.println(arr[i]);
+           }
+       }
+  }
