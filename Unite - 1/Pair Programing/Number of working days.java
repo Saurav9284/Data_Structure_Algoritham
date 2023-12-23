ee909bccba33579ac74351cc3bb2bd4a80049bd7
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-23 12:29:28 +0530

    Create Number of working days.java

---
+++ Unite - 1/Pair Programing/Number of working days.java
@@ -0,0 +1,9 @@
+public static void workingDays(int n){
+      int day=7;
+      int sum=0;
+      for(int i=1;i<=day-1;i++){
+          sum=sum+1;
+      }
+      System.out.println(n*sum);
+    
+    }
