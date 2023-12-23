b0d3e8f10ce9708151fb636bd0323543b344e886
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-23 12:32:45 +0530

    Create Characters At Odd Position.java

---
+++ Unite - 1/Strings/Characters At Odd Position.java
@@ -0,0 +1,10 @@
+public static void charAtOddPos(int n, String str){
+      
+      for(int i=0; i<n; i++)
+      {
+          if(i%2!=0)
+          {
+              System.out.println(str.charAt(i));
+          }
+      }
+  }
