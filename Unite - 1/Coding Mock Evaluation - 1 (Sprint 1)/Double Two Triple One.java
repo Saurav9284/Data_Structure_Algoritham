1119d1d8218fa6989a8a6364ff3dd3a7c7996e87
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-24 15:43:06 +0530

    Create Double Two Triple One.java

---
+++ Unite - 1/Coding Mock Evaluation - 1 (Sprint 1)/Double Two Triple One.java
@@ -0,0 +1,11 @@
+public static void doubleTwoTripleOne(int a,int b,int c,int d){
+        
+        int A= a*2;
+        int B= b*2;
+        int C= c*3;
+        int D= d+4;
+        
+        int sum = A+B+C+D;
+        
+        System.out.println(sum);
+    }
