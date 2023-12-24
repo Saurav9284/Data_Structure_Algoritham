0e34fafc9824ebc4d4f63c65661f2ceb003f29bd
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-24 15:44:01 +0530

    Create Print Grade.java

---
+++ Unite - 1/Coding Mock Evaluation - 1 (Sprint 1)/Print Grade.java
@@ -0,0 +1,19 @@
+public static void grade(int total){
+      
+      if(total==100)
+    {
+        System.out.println("A");
+    }
+    else if(total>=90)
+    {
+        System.out.println("B");
+    }
+    else if(total>=80)
+    {
+        System.out.println("C");
+    }
+    else
+    {
+        System.out.println("F");
+    }
+  }
