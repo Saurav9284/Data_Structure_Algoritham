35d4d21b53225b52c6c2b7a2777453d39400f0cf
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-25 10:36:31 +0530

    Create World Cup Final.java

---
+++ Unite - 1/Coding Mock Evaluation - 2 /World Cup Final.java
@@ -0,0 +1,28 @@
+public static void worldcupfinal(int NzScore,int NzSuperOver, int NzFours,int EngScore,int EngSuperOver,int EngFours){
+         
+         if(NzScore>EngScore)
+         {
+             System.out.println("New Zealand");
+         }
+         else if(NzScore<EngScore)
+         {
+             System.out.println("England");
+         }
+         else if(NzSuperOver>EngSuperOver)
+         {
+             System.out.println("New Zealand");
+         }
+         else if(NzSuperOver<EngSuperOver)
+         {
+             System.out.println("England");
+         }
+         else if(NzFours>EngFours)
+         {
+             System.out.println("New Zealand");
+         }
+         else if(NzFours<EngFours)
+         {
+             System.out.println("England");
+         }
+         
+    }
