7c9afd203814b53d80c4eaa1990136c44778839b
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-25 10:40:27 +0530

    Create Identify Prime.java

---
+++ Unite - 1/Coding Mock Evaluation - 2 /Identify Prime.java
@@ -0,0 +1,19 @@
+public static void identifyprime(int num){
+        
+         int count=0;
+          for(int i=1; i<=num; i++)
+          {
+              if(num%i==0)
+              {
+                  count++;
+              }
+          }
+          if(count==2)
+          {
+              System.out.println("Yes");
+          }
+          else
+          {
+              System.out.println("No");
+          }
+    }
