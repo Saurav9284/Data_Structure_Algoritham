2c060f469980ed7b8b20cf6eaa309d56b0ffdb0a
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-26 11:08:41 +0530

    Create Maximum of three.java

---
+++ Unite - 1/Coding Sprint Evaluation 1/Maximum of three.java
@@ -0,0 +1,17 @@
+public static void solve(int a, int b, int c){
+    
+    int max=0;
+    if(a>max)
+    {
+        max=a;
+    }
+    if(b>max)
+    {
+        max=b;
+    }
+    if(c>max)
+    {
+        max=c;
+    }
+    System.out.println(max);
+  }
