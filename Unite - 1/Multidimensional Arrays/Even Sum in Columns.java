821e5daaf7e389dc86fa08ffabe916d5b94c20e2
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-27 09:23:46 +0530

    Create Even Sum in Columns.java

---
+++ Unite - 1/Multidimensional Arrays/Even Sum in Columns.java
@@ -0,0 +1,16 @@
+public static void evenSumColumns(int rows, int cols, int mat[][]){
+       
+       for(int j=0; j<cols; j++)
+       {
+           int sum=0;
+           for(int i=0; i<rows; i++)
+           {
+               if(mat[i][j]%2==0)
+               {
+                   sum=sum+mat[i][j];
+               }
+           }
+           
+           System.out.println(sum);
+       }
+  }
