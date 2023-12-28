8a214e981b353544be464798140523af4037daf5
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-28 09:25:18 +0530

    Create Product of Seven.java

---
+++ Unite - 1/Coding Mock Evaluation - 1(Sprint 3) /Product of Seven.java
@@ -0,0 +1,7 @@
+public static void productOfSeven(int one,int two,int three,int four,int five,int six,int seven){
+    
+    long sum = (long)one*two*three*four*five*six*seven;
+    
+    System.out.println(sum);
+    
+  }
