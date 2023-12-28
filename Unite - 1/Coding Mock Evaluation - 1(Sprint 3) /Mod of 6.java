88fd15465df0462492b67fb3416b814234e5427d
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-28 09:26:17 +0530

    Create Mod of 6.java

---
+++ Unite - 1/Coding Mock Evaluation - 1(Sprint 3) /Mod of 6.java
@@ -0,0 +1,27 @@
+public static void modOf6(int num){
+    
+     if(num%6==0)
+     {
+         System.out.println("zero");
+     }
+     if(num%6==1)
+     {
+         System.out.println("one");
+     }
+     if(num%6==2)
+     {
+         System.out.println("two");
+     }
+     if(num%6==3)
+     {
+         System.out.println("three");
+     }
+     if(num%6==4)
+     {
+         System.out.println("four");
+     }
+     if(num%6==5)
+     {
+         System.out.println("five");
+     }
+  }
