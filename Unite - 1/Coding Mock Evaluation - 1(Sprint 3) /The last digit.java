d0859925dddd815d03d622775babcd1e92231928
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-28 09:25:46 +0530

    Create The last digit.java

---
+++ Unite - 1/Coding Mock Evaluation - 1(Sprint 3) /The last digit.java
@@ -0,0 +1,15 @@
+public static void theLastDigit(int number){
+       
+       int lastdigit = number%10;
+       
+       if(lastdigit%3==0)
+       {
+           System.out.println("Divisible");
+       }
+       else
+       {
+            System.out.println("Not Divisible");
+       }
+       
+       
+  }
