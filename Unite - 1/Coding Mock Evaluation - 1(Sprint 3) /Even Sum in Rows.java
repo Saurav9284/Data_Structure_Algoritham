691cc119d6085f9a630ce588e9cc72e7ba82b6e8
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-28 09:30:15 +0530

    Create Even Sum in Rows.java

---
+++ Unite - 1/Coding Mock Evaluation - 1(Sprint 3) /Even Sum in Rows.java
@@ -0,0 +1,16 @@
+public static void evenSumInRows(int n,int m,int[][] arr){
+    
+      for(int i=0; i<n; i++)
+      {
+          int sum=0;
+          for(int j=0; j<m; j++)
+          {
+              if(arr[i][j]%2==0)
+              {
+                  sum=sum+arr[i][j];
+              }
+          }
+          
+          System.out.println(sum);
+      }
+  }
