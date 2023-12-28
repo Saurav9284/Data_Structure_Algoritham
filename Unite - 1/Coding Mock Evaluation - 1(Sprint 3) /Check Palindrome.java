bc8a31544f48ef7860c3e2dc4fe2279f5569fb45
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-28 09:29:46 +0530

    Create Check Palindrome.java

---
+++ Unite - 1/Coding Mock Evaluation - 1(Sprint 3) /Check Palindrome.java
@@ -0,0 +1,16 @@
+public static void checkPalindrome(int n, String str){
+    String reverse="";
+    
+    for(int i=n-1; i>=0; i--)
+    {
+        reverse=reverse+str.charAt(i);
+    }
+    if(str.equals(reverse))
+    {
+        System.out.println("Yes");
+    }
+    else
+    {
+        System.out.println("No");
+    }
+  }
