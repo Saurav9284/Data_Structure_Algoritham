188857b3efdc7e29aabb54b512f80a84b8073cfa
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-28 09:30:41 +0530

    Create Even Sum in Columns.java

---
+++ Unite - 1/Coding Mock Evaluation - 1(Sprint 3) /Even Sum in Columns.java
@@ -0,0 +1,17 @@
+public static void evenSumInColumns(int n,int m,int[][] arr){
+      
+      
+       for(int j=0; j<m; j++)
+       {
+           int sum =0;
+           for(int i=0; i<n; i++)
+           {
+               if(arr[i][j]%2==0)
+               {
+                   sum=sum+arr[i][j];
+               }
+           }
+           
+           System.out.println(sum);
+       }
+  }
