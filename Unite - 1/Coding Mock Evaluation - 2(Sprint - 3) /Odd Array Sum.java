a1081aabec5a196c9ff8141433f7ff4d53bd5313
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-30 21:36:46 +0530

    Create Odd Array Sum.java

---
+++ Unite - 1/Coding Mock Evaluation - 2(Sprint - 3) /Odd Array Sum.java
@@ -0,0 +1,14 @@
+public static void oddArraySum(int n, int arr[]){
+      
+      int sum=0;
+      
+      for(int i=0; i<n; i++)
+      {
+          if(arr[i]%2!=0)
+          {
+              sum=sum+arr[i];
+          }
+      }
+      
+      System.out.println(sum);
+  }
