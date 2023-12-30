19eefd7e5d1bdd7bb606f4976f50bb86fdd78409
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2023-12-30 21:28:22 +0530

    Create Pattern of N.java

---
+++ Unite - 1/Coding Mock Evaluation - 2(Sprint - 3) /Pattern of N.java
@@ -0,0 +1,14 @@
+public static void patternOfN(int N){
+     
+       int number=1;
+       for(int i=1; i<=N; i++)
+       {
+           for(int j=1; j<=N; j++)
+           {
+               System.out.print(number+" ");
+               number++;
+           }
+           
+           System.out.println();
+       }
+  }
