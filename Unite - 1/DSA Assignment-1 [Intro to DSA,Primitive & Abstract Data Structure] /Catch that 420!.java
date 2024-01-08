bbf96e5c8afa8e9685fa258e74fa2d2ffedeb541
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-08 10:58:46 +0530

    Create Catch that 420!.java

---
+++ Unite - 1/DSA Assignment-1 [Intro to DSA,Primitive & Abstract Data Structure] /Catch that 420!.java
@@ -0,0 +1,14 @@
+public static void solve(int n){
+        //write your code here
+        
+        String z= Integer.toString(n);
+        
+        if(z.contains("420"))
+        {
+            System.out.println("Caught");
+        }
+        else
+        {
+            System.out.println("Escaped");
+        }
+    }
