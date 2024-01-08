e6042a923948e85784f26aa365ce86b3c8ee9303
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-08 11:00:32 +0530

    Create Masai Sentence Reversal.java

---
+++ Unite - 1/DSA Assignment-1 [Intro to DSA,Primitive & Abstract Data Structure] /Masai Sentence Reversal.java
@@ -0,0 +1,10 @@
+public static void masaiSentenceReverse(String s){
+  
+      String[]arr = s.split(" ");
+      
+      for(int i= arr.length-1; i>=0; i--)
+      {
+          System.out.print(arr[i]+" ");
+      }
+      
+  }
