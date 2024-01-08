064927cfc6fcf2cfc3843aae4c12bdf4f64d6554
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-08 10:59:23 +0530

    Create Count Consonant.java

---
+++ Unite - 1/DSA Assignment-1 [Intro to DSA,Primitive & Abstract Data Structure] /Count Consonant.java
@@ -0,0 +1,16 @@
+public static void countConsonants(String s){
+    
+       int count = 0;
+       
+       
+       for(int i = 0; i < s.length(); i++)
+       {
+           if(s.charAt(i)!= 'a' && s.charAt(i)!= 'e' && s.charAt(i)!= 'i' && s.charAt(i)!= 'o' && s.charAt(i)!= 'u')
+           {
+           count++;
+           }
+           
+       }
+       
+       System.out.println(count);
+  }
