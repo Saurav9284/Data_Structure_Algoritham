18a8c036a8aa4f096825405b1aa792d4c1bd2fc4
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-08 11:01:49 +0530

    Create Compete with Neighbour.java

---
+++ Unite - 1/DSA Assignment-1 [Intro to DSA,Primitive & Abstract Data Structure] /Compete with Neighbour.java
@@ -0,0 +1,26 @@
+public static void solve(int n,int[] arr){
+    int count =0;
+    
+    if(arr[0]>arr[1])
+    {
+        count++;
+    }
+    for(int i=1; i<n-1; i++)
+    {
+        if(arr[i]>arr[i+1] && arr[i]>arr[i-1])
+        {
+            count++;
+        }
+    }
+    if(arr[n-1]>arr[n-2])
+    {
+        count++;
+    }
+    
+    System.out.println(count);
+  }
+ Overview
+ Announcements
+ Problems
+ Submissions
+ Rankings
