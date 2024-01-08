d3b0982d6d8b7581f3ccdea8c9eb9dceedeacf8c
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-08 10:55:24 +0530

    Create Masai School! Hurray!.java

---
+++ Unite - 1/DSA Assignment-1 [Intro to DSA,Primitive & Abstract Data Structure] /Masai School! Hurray!.java
@@ -0,0 +1,19 @@
+public static void masaiSchoolHurray(int n){
+      
+      if(n%35==0)
+      {
+          System.out.println("Masai School");
+      }
+      else if(n%7==0)
+      {
+          System.out.println("Masai");
+      }
+      else if(n%5==0)
+      {
+          System.out.println("School");
+      }
+      else
+      {
+          System.out.println("Hurray!");
+      }
+  }
