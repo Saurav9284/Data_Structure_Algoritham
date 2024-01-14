b470198ab50d0a3678400ebbc71f3ca24238881e
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-14 15:05:40 +0530

    Create Compete with Neighbour.java

---
+++ Unite - 1/DSA Assignment-4 [Arrays & Strings-PSC] /Compete with Neighbour.java
@@ -0,0 +1,24 @@
+public static void competeWithNeighbour(int N, int A[]){
+      
+      int count =0;
+      
+      if(A[0]>A[1])
+      {
+          count++;
+      }
+      
+      for(int i=1; i<N-1; i++)
+      {
+          if(A[i]>A[i-1] && A[i]>A[i+1])
+          {
+              count++;
+          }
+      }
+      
+      if(A[N-1]>A[N-2])
+      {
+          count++;
+      }
+      
+      System.out.println(count);
+  }
