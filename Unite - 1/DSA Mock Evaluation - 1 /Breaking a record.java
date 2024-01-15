b3d5711c5a174e0e522e7805ebd6310de01b8e63
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-15 12:25:52 +0530

    Create Breaking a record.java

---
+++ Unite - 1/DSA Mock Evaluation - 1 /Breaking a record.java
@@ -0,0 +1,15 @@
+public static void breakingRecord(int n,int m){
+        
+        if(m>n)
+        {
+            System.out.println("Broken");
+        }
+        else if(m<n)
+        {
+            System.out.println("Not Yet");
+        }
+        else if(m==n)
+        {
+            System.out.println("Wao");
+        }
+    }
