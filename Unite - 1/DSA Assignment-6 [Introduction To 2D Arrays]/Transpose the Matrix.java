8f536f31e6ca7fabcb418c2986916a6c167f1dcd
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-19 11:45:01 +0530

    Create Transpose the Matrix.java

---
+++ Unite - 1/DSA Assignment-6 [Introduction To 2D Arrays]/Transpose the Matrix.java
@@ -0,0 +1,12 @@
+public static void solve(int n,int m, int[][] arr){
+    //write your code here
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            System.out.print(arr[j][i]+" ");
+        }
+        
+        System.out.println();
+    }
+  }
