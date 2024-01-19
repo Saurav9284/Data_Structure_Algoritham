8c16de2b193387b2ed68ed8d47ad27b12012a286
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-19 11:43:11 +0530

    Update Traverse a 2D Array.java

--- Unite - 1/DSA Assignment-6 [Introduction To 2D Arrays]/Traverse a 2D Array.java
+++ Unite - 1/DSA Assignment-6 [Introduction To 2D Arrays]/Traverse a 2D Array.java
@@ -1,1 +1,12 @@
 
+public static void solve(int n,int m,int[][] arr){
+    
+    
+     for(int i = 0; i < m; i++)
+     {
+         for(int j = n-1; j>=0; j--)
+         {
+             System.out.print(arr[j][i]+ " ");
+         }
+     }
+  }
