5a8ce230773635d8736f8f8896bb9e9ba1021708
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-20 12:01:23 +0530

    Create Vowel & Consonant Count.java

---
+++ Unite - 1/DSA Unit Evaluation /Vowel & Consonant Count.java
@@ -0,0 +1,21 @@
+public static void solve(int n, String str){
+      
+      int count=0;
+      int count1=0;
+      
+      for(int i=0; i<n; i++)
+      {
+          char z= str.charAt(i);
+          
+          if(z=='a'||z=='e'||z=='i'||z=='o'||z=='u')
+          {
+              count=count+1;
+          }
+          else
+          {
+              count1=count1+1;
+          }
+      }
+      
+      System.out.println(count+" "+count1);
+  }
