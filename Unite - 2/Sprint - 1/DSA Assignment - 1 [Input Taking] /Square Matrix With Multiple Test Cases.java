029ade2b125e72c9daef82aee06d71205c786219
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-22 12:04:02 +0530

    Create Square Matrix With Multiple Test Cases.java

---
+++ Unite - 2/Sprint - 1/DSA Assignment - 1 [Input Taking] /Square Matrix With Multiple Test Cases.java
@@ -0,0 +1,30 @@
+import java.util.*;
+  
+  public class Main{
+       
+       public static void main (String []argd){
+           
+           Scanner sc = new Scanner(System.in);
+           
+           int t = sc.nextInt();
+           
+           for(int k=0; k<t; k++)
+           {
+                int n = sc.nextInt();
+          
+          int [][]array = new int [n][n];
+          
+          for(int i=0; i<n; i++)
+          {
+              for(int j=0; j<n; j++)
+              {
+                  array[i][j]=sc.nextInt();
+                  
+                  System.out.print(array[i][j]+1+" ");
+              }
+              
+              System.out.println();
+          }
+           }
+       }
+  }
