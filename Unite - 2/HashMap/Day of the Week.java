a3d3a330b202352b8293304b95f92f47ee3bfdd6
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-23 13:07:42 +0530

    Create Day of the Week.java

---
+++ Unite - 2/HashMap/Day of the Week.java
@@ -0,0 +1,40 @@
+ import java.util.*;
+ 
+ public class Main {
+     
+     public static void main(String[]args) {
+         
+         Scanner sc = new Scanner(System.in);
+         
+         int n = sc.nextInt();
+         
+         if(n==0)
+         {
+             System.out.println("Sunday");
+         }
+         if(n==1)
+         {
+             System.out.println("Monday");
+         }
+         if(n==2)
+         {
+             System.out.println("Tuesday");
+         }
+         if(n==3)
+         {
+             System.out.println("Wednesday");
+         }
+         if(n==4)
+         {
+             System.out.println("Thursday");
+         }
+         if(n==5)
+         {
+             System.out.println("Friday");
+         }
+         if(n==6)
+         {
+             System.out.println("Saturday");
+         }
+     }
+ }
