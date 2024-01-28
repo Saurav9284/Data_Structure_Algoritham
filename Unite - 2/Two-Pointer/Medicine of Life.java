fa184244b30e0680b29b802f27290ef4ff6f0f1e
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-01-28 18:41:33 +0530

    Create Medicine of Life.java

---
+++ Unite - 2/Two-Pointer/Medicine of Life.java
@@ -0,0 +1,98 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    
+   static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+ 
+        public FastReader()
+        {
+            br = new BufferedReader(
+                new InputStreamReader(System.in));
+        }
+ 
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() { return Integer.parseInt(next()); }
+ 
+        long nextLong() { return Long.parseLong(next()); }
+ 
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+ 
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    
+    public static void main(String args[] ){
+    
+      FastReader sc = new FastReader();
+        
+        int t = sc.nextInt();
+        
+        for(int a=0; a<t; a++)
+        {
+            int n = sc.nextInt();
+            
+            int k = sc.nextInt();
+            
+            int arr[] = new int [n];
+            
+            for(int i=0; i<n; i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+            
+            int i=0;
+            int j=n-1;
+            String ans = "Impossible";
+            
+            while(i<j)
+            {
+                int sum = 0;
+                
+                sum = arr[i] + arr[j];
+                
+                if(sum<k)
+                {
+                    i++;
+                }
+                else if(sum>k)
+                {
+                    j--;
+                }
+                else
+                {
+                    ans = "Possible";
+                    break;
+                }
+            }
+            
+            System.out.println(ans);
+        }
+    }
+}
