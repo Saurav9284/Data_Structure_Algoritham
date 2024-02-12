efb16ae383b83ba5e7570a968db697f7c98eda2e
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-02-12 13:05:28 +0530

    Create You have to sort it out!.java

---
+++ Unite - 2/You have to sort it out!.java
@@ -0,0 +1,48 @@
+import java.util.*;
+ 
+ public class Main{
+     
+     public static void main(String[]args){
+         
+         Scanner sc = new Scanner(System.in);
+         
+         int n = sc.nextInt();
+         
+         int arr [] = new int [n];
+         
+         for(int i=0; i<n; i++)
+         {
+             arr[i] = sc.nextInt();
+         }
+         
+         int ans [] = new int [n];
+         
+         for(int i=0; i<n; i++)
+         {
+             ans[i] = i;
+         }
+         
+         for(int i=0; i<n-1; i++)
+         {
+             for(int j=0; j<n-i-1; j++)
+             {
+                 if(arr[j]>arr[j+1])
+                 {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    
+                    int tempo = ans[j];
+                    ans[j] = ans[j+1];
+                    ans[j+1] = tempo;
+                 }
+             }
+             
+         }
+         
+         for(int i : ans)
+         {
+             System.out.print(i+" ");
+         }
+     }
+ }
