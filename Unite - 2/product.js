8103a7ef7eb9454532479d3a6d53b12dd480c307
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-02-13 11:27:11 +0530

    Update product.js

--- Unite - 2/product.js
+++ Unite - 2/product.js
@@ -164,3 +164,26 @@ productController.get('/products', async (req, res) => {
   productController.use(errorHandler);
 
   module.exports = productController
+
+
+
+
+//Middleware-> auth.js
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../Config/db')
+
+const authenticateUser = (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.send({ msg: 'Login first' });
+    }
+    const token = req.headers.authorization.split("Bearer ")[1];
+    jwt.verify(token,JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+      req.userId = decoded.userId;
+      next();
+    });
+  };
+
+  module.exports = authenticateUser
