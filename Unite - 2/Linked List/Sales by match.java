9ae4e8861e1623fb6d443ac9894297d30552dfe1
Saurav Sanjay Chavan <saurav178chavan@gmail.com>
2024-02-20 15:59:14 +0530

    Create Sales by match.java

---
+++ Unite - 2/Linked List/Sales by match.java
@@ -0,0 +1,33 @@
+import java.util.*;
+ 
+ class Main{
+     
+     public static void main(String[]args){
+         
+         Scanner sc = new Scanner(System.in);
+         
+         int n = sc.nextInt();
+         
+         int [] arr = new int [n];
+         
+         for(int i=0; i<n; i++)
+         {
+             arr[i] = sc.nextInt();
+         }
+         
+         Arrays.sort(arr);
+         
+         int count = 0;
+         
+         for(int i=0; i<n-1; i++)
+         {
+             if(arr[i]==arr[i+1])
+             {
+                 count++;
+                 i++;
+             }
+         }
+         
+         System.out.println(count);
+     }
+ }
